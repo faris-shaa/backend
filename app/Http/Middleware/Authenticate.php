c7b37171f25a3f821803c421cf0d6209ce4e6983
Yasser Ismail <yismail@prexle.com>
2024-12-17 13:17:55 +0200

    edit login problem

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 
 class Authenticate extends Middleware
@@ -12,14 +13,19 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        
-        if (! $request->expectsJson()) {
-            return route('login');
+
+        if (!$request->expectsJson()) {
+            if (Str::contains($request->fullUrl(), "admin")) {
+                return route("admin.login");
+            } elseif (Str::contains($request->fullUrl(), "organizers")) {
+                return  route("organizers.login");
+            }
+            return route('home');
         }
     }
 }
